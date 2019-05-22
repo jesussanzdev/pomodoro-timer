9fb469832be2ec86bb498d6bc790fc951f8a3da1
Jesus Sanz <jesussanz77@gmail.com>
2019-05-22 16:17:11 +0200

    Updates

---
+++ precache-manifest.9a17de2a602a5a27149e27014ade1fdf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4d4f6002dd1de7a938bce2fab38d24",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "d90ce2a2bdc4aaa63072",
+    "url": "/pomodoro-timer/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a1d80fbee267c46bbabe",
+    "url": "/pomodoro-timer/static/css/main.7f266502.chunk.css"
+  },
+  {
+    "revision": "d90ce2a2bdc4aaa63072",
+    "url": "/pomodoro-timer/static/js/2.da9fbdb7.chunk.js"
+  },
+  {
+    "revision": "a1d80fbee267c46bbabe",
+    "url": "/pomodoro-timer/static/js/main.6c653e6e.chunk.js"
+  },
+  {
+    "revision": "8d84c672a99e0f0ece83",
+    "url": "/pomodoro-timer/static/js/runtime~main.2a0df6ba.js"
+  },
+  {
+    "revision": "988833bf34d92eba3d536029cd9e1bc6",
+    "url": "/pomodoro-timer/static/media/BeepSound.988833bf.wav"
+  },
+  {
+    "revision": "8e8e625f12e45ecc674f9579c4547351",
+    "url": "/pomodoro-timer/static/media/DS-DIGII.8e8e625f.TTF"
+  }
+]);
